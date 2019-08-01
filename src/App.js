fa693be3a70ab43cd01b77d23bd4671f58cda690
Vasyl Butov <loctary@mail.ru>
2019-08-01 11:12:18 +0300

    types added

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
 import React, { Component } from 'react';
-import Immutable from 'immutable';
+import { Record, Set, List } from 'immutable';
 import response from './response';
 
-// const DocumentFactory: RecordFactory<Document> = new Record({
-//   documentID: '',
-//   created: '',
-//   doc: '',
-//   docStatus: '',
-//   finInfo: null,
-//   sourceID: null,
-//   versionID: null,
-//   folderID: null,
-//   new: 0,
-//   notes: '',
-//   source: '',
-//   total: 0,
-//   type: 2,
-//   tags: new Set(),
-//   recentTags: new Set(),
-//   doctype: 'general',
-//   status: ['new'], // TODO: Remove synthetic attribute
-//   reason: '',
-//   protected: false,
-//   viewinfo: 'ViewinfoFactory()',
-//   linkid: '',
-//   ltext: '',
-//   isBeingProcessed: false,
-// });
+type ViewinfoTypes = {
+  pages: number,
+  rotations: List<int>,
+};
 
-// для каждого item из json создается такая фабрика
-// DocumentFactory() - отдает рекорд с дефолтными значениями
-// DocumentFactory(item) -  отдает рекорд на основании объекта item, если в item
-// нет определенных в рекорде ключей, ключи получают дефолтные значения
-// у тебя получится такая фабрика для каждого рекорда который я описал в
-// структуре. Их надо скомпоновать чтобы получить в итоге рекорд с полями среди
-// которых будут другие рекорды
+type ResponseTypes = {
+  ltext: string,
+  created: string,
+  sourceID: string,
+  linkid: string,
+  notes: string,
+  tags: Set<string>,
+  reason: string,
+  versionID: string,
+  details: string,
+  finInfo: any,
+  viewinfo: Record<ViewinfoTypes>,
+  documentID: string,
+};
+
+const ViewinfoFactory: ViewinfoTypes = new Record({
+  pages: 0,
+  rotations: new List(),
+});
+
+const DocumentFactory: ResponseTypes = new Record({
+  ltext: '',
+  created: '',
+  sourceID: '',
+  linkid: '',
+  notes: '',
+  tags: new Set(),
+  reason: '',
+  versionID: '',
+  details: '',
+  finInfo: null,
+  viewinfo: ViewinfoFactory(),
+  documentID: '',
+});
 
 class App extends Component {
   componentDidMount() {
-    const Res = Immutable.Record(response);
+    const Res = Record(response);
     const res = new Res();
-    console.log(res);
+    console.log(res.items.map(DocumentFactory));
   }
 
   render() {
-    return <div className="App" />;
+    return <div />;
   }
 }
 
